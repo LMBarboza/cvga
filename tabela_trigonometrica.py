009c9830a2d9d8e2f87e6e54818c4baf488dfe14
LMBarboza <leon.uchoa@gmail.com>
2024-07-14 13:09:57 -0300

    add tabela_trigonometrica

---
+++ tabela_trigonometrica.py
@@ -0,0 +1,25 @@
+import math
+
+
+def sin(a: int, b: int, senos: dict, cosenos: dict) -> float:
+    return senos[a] * cosenos[b] + cosenos[a] * senos[b]
+
+
+def cos(a: int, b: int, senos: dict, cosenos: dict) -> float:
+    return cosenos[a] * cosenos[b] - senos[a] * senos[b]
+
+
+def main() -> None:
+    sin_1 = math.sin(math.radians(1))
+    cos_1 = math.cos(math.radians(1))
+    senos = {1: sin_1}
+    cosenos = {1: cos_1}
+
+    for i in range(1, 90):
+        senos[i + 1] = sin(i, 1, senos, cosenos)
+        cosenos[i + 1] = cos(i, 1, senos, cosenos)
+    print(cosenos)
+
+
+if __name__ == "__main__":
+    main()
